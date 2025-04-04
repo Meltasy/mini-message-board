d85002901a819b76d1b613dc520542c2cbbe02cf
Meltasy <119603631+Meltasy@users.noreply.github.com>
2025-04-04 14:07:56 +0200

    Removed connection string

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,34 +1,34 @@
 #! /usr/bin/env node
 
-require('dotenv').config()
-const { Client } = require('pg')
+// require('dotenv').config()
+// const { Client } = require('pg')
 
-const SQL = `
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR ( 255 ),
-  messagetext VARCHAR ( 255 ),
-  added TIMESTAMP
-);
+// const SQL = `
+// CREATE TABLE IF NOT EXISTS messages (
+//   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//   username VARCHAR ( 255 ),
+//   messagetext VARCHAR ( 255 ),
+//   added TIMESTAMP
+// );
 
-INSERT INTO messages (username, messagetext, added)
-VALUES
-  ('Waly Dia', 'What''s up, guys?', NOW()),
-  ('Guillaume Meurice', 'In the news this week ...', NOW()),
-  ('Juliette Arnaud', 'Time for a story!', NOW()),
-  ('Pierre-Emmanuel Barré', 'Come to Saint Roustan.', NOW())
-`;
+// INSERT INTO messages (username, messagetext, added)
+// VALUES
+//   ('Waly Dia', 'What''s up, guys?', NOW()),
+//   ('Guillaume Meurice', 'In the news this week ...', NOW()),
+//   ('Juliette Arnaud', 'Time for a story!', NOW()),
+//   ('Pierre-Emmanuel Barré', 'Come to Saint Roustan.', NOW())
+// `;
 
-async function main() {
-  console.log('seeding...')
-  console.log('Connection String:', process.env.CONNECTION_STRING)
-  const client = new Client({
-    connectionString: process.env.CONNECTION_STRING
-  })
-  await client.connect()
-  await client.query(SQL)
-  await client.end()
-  console.log('done')
-}
+// async function main() {
+//   console.log('seeding...')
+//   console.log('Connection String:', process.env.CONNECTION_STRING)
+//   const client = new Client({
+//     connectionString: process.env.CONNECTION_STRING
+//   })
+//   await client.connect()
+//   await client.query(SQL)
+//   await client.end()
+//   console.log('done')
+// }
 
-main()
+// main()
